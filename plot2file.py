58dd770e5b28e796805a04d147b02e1515abbe50
Benno Kutschank <benku@live.de>
2024-07-26 21:57:35 +0200

    recognizing axes

--- plot2file.py
+++ plot2file.py
@@ -1,6 +1,8 @@
-# parameters:
+# TODO:
+# split up axes
 # x and y sampling
 # dealing with time, dates, labels
+# logarithmic scale
 
 import argparse
 import cv2
@@ -13,13 +15,13 @@ parser.add_argument('filename')
 #args = parser.parse_args()
 
 #img = cv2.imread(args.filename)
-img = cv2.imread("./data/example.png")
+img = cv2.imread("./data/example2.png")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 height, width = img.shape[:2]
 # this value determines which lines are eligible to be considered as an axis
 # a value of e.g. 0.3 means that a line's length must be at least 30% of the image size to be a candidate for an axis
-axis_threshold = 0.3
+axis_threshold = 0.4
 line_threshold_x = width * axis_threshold
 line_threshold_y = height * axis_threshold
 line_threshold = min(line_threshold_x, line_threshold_y)
@@ -41,8 +43,10 @@ min_line_length = line_threshold  # minimum number of pixels making up a line
 max_line_gap = 10  # maximum gap in pixels between connectable line segments
 line_image = np.copy(img) * 0
 
-lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-                    min_line_length, max_line_gap)
+lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+
+x_lines = []
+y_lines = []
 
 for line in lines:
     for x1,y1,x2,y2 in line:
@@ -50,10 +54,53 @@ for line in lines:
         dy = y2 - y1
         angle = np.degrees(np.arctan2(dy, dx))
         angle = abs(angle % 180)
-        if abs(angle) <= axis_aligned_threshold or abs(angle - 90) <= axis_aligned_threshold:
-            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),3)
+        if abs(angle) <= axis_aligned_threshold:
+            x_lines.append((x1, y1, x2, y2))
+        elif abs(angle - 90) <= axis_aligned_threshold:
+            y_lines.append((x1, y1, x2, y2))
         else:
-            cv2.line(line_image,(x1,y1),(x2,y2),(0,255,0),3)
+            pass
+            #cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 16)
+
+# find the most extreme coordinates for the axes
+x_axis = max(x_lines, key=lambda ele: ele[1])
+y_axis = min(y_lines, key=lambda ele: ele[0])
+x_axis_y = x_axis[1]
+y_axis_x = y_axis[0]
+# sometimes, the lines in x/y_axis don't stretch along the entire graph
+# therefore, we need to find other lines at similar coordinates and amalgate the result into a synthetic line
+x_axis_y_stretch = x_axis_y * 0.05
+y_axis_x_stretch = y_axis_x * 0.05
+x_axis_min = width
+x_axis_max = 0
+y_axis_min = height
+y_axis_max = 0
+for line in x_lines:
+    x1, y1, x2, y2 = line
+    if y1 > x_axis_y - x_axis_y_stretch and y1 < x_axis_y + x_axis_y_stretch:
+        x_axis_min = min(x_axis_min, x1)
+        x_axis_max = max(x_axis_max, x2)
+for line in y_lines:
+    x1, y1, x2, y2 = line
+    if x1 > y_axis_x - y_axis_x_stretch and x1 < y_axis_x + y_axis_x_stretch:
+        y_axis_min = min(y_axis_min, y1)
+        y_axis_max = max(y_axis_max, y2)
+x_axis_min, x_axis_max = min(x_axis_min, x_axis_max), max(x_axis_min, x_axis_max)
+y_axis_min, y_axis_max = min(y_axis_min, y_axis_max), max(y_axis_min, y_axis_max)
+cv2.line(line_image, [x_axis_min, x_axis_y], [x_axis_max, x_axis_y], (32, 240, 160), 1)
+cv2.line(line_image, [y_axis_x, y_axis_min], [y_axis_x, y_axis_max], (32, 240, 160), 1)
+
+lines = cv2.HoughLinesP(edges, rho, theta, 5, np.array([]), max_line_gap, max_line_gap)
+
+for line in lines:
+    for x1, y1, x2, y2 in line:
+        x1_t, x2_t = min(x1, x2), max(x1, x2)
+        y1_t, y2_t = min(y1, y2), max(y1, y2)
+        if x1_t > x_axis_min and x2_t < x_axis_max and y1_t > y_axis_min and y2_t < y_axis_max:
+            cv2.line(line_image, [x1, y1], [x2, y2], (255, 0, 0), 1)
+
+edges = cv2.Canny(line_image, low_t, high_t)
+cv2.imshow("t", edges)
 
 img_lines = cv2.addWeighted(img, 0.8, line_image, 1, 0)
 
